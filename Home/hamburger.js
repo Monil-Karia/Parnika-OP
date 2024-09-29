54cd0dd9d0f1262f3d75116d2949b8847959211b
monil_karia <monilkaria2019@gmail.com>
2024-09-29 11:55:28 +0530

    Footer and minor changes

--- Home/hamburger.js
+++ Home/hamburger.js
@@ -17,24 +17,16 @@ const changingPart = document.getElementById("changing-part");
 let wordIndex = 0;
 
 function changeText() {
-    // Apply fade-out class to the current text
     changingPart.classList.add("fade-up-out");
-
     setTimeout(() => {
-        // Update the text content after fade-out
         wordIndex = (wordIndex + 1) % changingWords.length;
         changingPart.textContent = changingWords[wordIndex];
-
-        // Remove fade-out class and apply fade-in class
         changingPart.classList.remove("fade-up-out");
         changingPart.classList.add("fade-up-in");
-
-        // Set a timeout to remove fade-in class after the animation
         setTimeout(() => {
             changingPart.classList.remove("fade-up-in");
-        }, 1000); // Match this duration with the fade-in animation duration
-    }, 1000);  // Match this duration with the fade-out animation duration
+        }, 1000);
+    }, 1000);  
 }
-
 // Run the text change every 3 seconds
 setInterval(changeText, 3000);
